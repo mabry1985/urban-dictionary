8753d012f225dac555c34bcf9b62ef5cd6e42a0b
Abdellah Ramadan, Josh Mabry and Sam Cook <josh@>
2019-07-03 16:28:55 -0700

    add more stuff

--- src/index.js
+++ src/index.js
@@ -7,11 +7,31 @@ $(document).ready(function () {
     $('#word').val('');
     let urbanService = new UrbanDictionary();
     let promise = urbanService.getUrbanDefinition(word);
+    let promiseTwo = urbanService.getTvShow(word);
+    let promiseThree = urbanService.goAway(word);
     promise.then(function (response) {
       let body = JSON.parse(response);
       $('.showWord').text(body.list[0].word);
       $('.showDefinition').text(body.list[0].definition);
       $('.showExample').text(body.list[0].example);
+    });
+    promiseTwo.then(function (response) {
+      let body = JSON.parse(response);
+      $('.showName').html(body[0].show.name);
+      $('.showRating').html(body[0].show.rating);
+      $('.showPremiered').html(body[0].show.premiered);
+      $('.showCountry').html(body[0].show.network.country.name);
+      $('.showSummary').html(body[0].show.summary);
+
+    });
+
+    promiseThree.then(function (response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      
+      $('.showInsult').html(body.message);
+
+
     });
   });
 
--- src/urban-dictionary.js
+++ src/urban-dictionary.js
@@ -1,4 +1,5 @@
 export class UrbanDictionary {
+
   getUrbanDefinition(word) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
@@ -14,5 +15,40 @@ export class UrbanDictionary {
       request.open('GET', url, true);
       request.send();
     });
+  }
+
+  getTvShow(word) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let tv = `http://api.tvmaze.com/search/shows?q=${word}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+
+      request.open('GET', tv, true);
+      request.send();
+    });
+  }
+  goAway(word) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://www.foaas.com/${word}/Sam`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+
+      };
+      request.open('GET', url, true);
+      request.setRequestHeader('Accept', 'application/json');
+      request.send();
+    });
+
   }
 }
